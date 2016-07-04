59443a8aa990d7ab74ed6e710ae042248cd62b8b
Dan Poggi <dan.poggi@gmail.com>
2016-07-04 15:03:25 -0400

    Add wildfly10.rb, update LICENSE.

---
+++ wildfly10.rb
@@ -0,0 +1,27 @@
+class Wildfly10 < Formula
+  desc "Managed Java EE application server/runtime."
+  homepage "http://wildfly.org/"
+  url "https://download.jboss.org/wildfly/10.0.0.Final/wildfly-10.0.0.Final.tar.gz"
+  sha256 "e00c4e4852add7ac09693e7600c91be40fa5f2791d0b232e768c00b2cb20a84b"
+
+  bottle :unneeded
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    rm_f Dir["bin/*.ps1"]
+    libexec.install Dir["*"]
+  end
+
+  def caveats; <<-EOS.undent
+    The home of WildFly #{version} is:
+      #{opt_libexec}
+    You may want to add the following to your .bash_profile:
+      export JBOSS_HOME=#{opt_libexec}
+      export PATH=${PATH}:${JBOSS_HOME}/bin
+    EOS
+  end
+
+  test do
+    system "#{opt_libexec}/bin/standalone.sh --version | grep #{version}"
+  end
+end
